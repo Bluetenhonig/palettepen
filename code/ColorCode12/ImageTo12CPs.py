46b21369bf655441dfcb84e5708f3f83ee9fff1d
Bluetenhonig <linda.samsinger@uzh.ch>
2020-06-26 13:59:11 +0200

    update general, machine learning and output images for Task 3

--- code/ColorCode12/ImageTo12CPs.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Mar 19 09:13:15 2020
-
-@author: lsamsi
-"""
-
-
-#TODO: input picture (face, interior design etc.), get out seasonal color analysis from it 
